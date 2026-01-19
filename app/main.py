89df3a638ce801f8f9dd7a117df0f72d7d8c9119
stadnikruslan168-hub <stadnikruslan168@gmail.com>
2026-01-19 18:50:07 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
-from pydoc import classify_class_attrs
-
-
 def get_human_age(cat_age: int, dog_age: int) -> list:
     human_age = [0, 0]
     if cat_age >= 15:
@@ -11,10 +8,9 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
         remaining1 -= 9
         extra_years_cats = remaining1 // 4
         human_age[0] += extra_years_cats
-
     if dog_age >= 15:
         human_age[1] += 1
-    remaining2 = dog_age -  15
+    remaining2 = dog_age - 15
     if remaining2 >= 9:
         human_age[1] += 1
         remaining2 -= 9
