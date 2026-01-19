6daa858f2be3d2c180297ccc91879d6e4addd734
stadnikruslan168-hub <stadnikruslan168@gmail.com>
2026-01-19 20:10:52 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,22 +2,24 @@ from app.main import get_human_age
 import pytest
 
 
-@pytest.mark.parametrize(
-    "cat_age, dog_age, human_age",
-    [
-        (0, 0, [0, 0]),
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-        (24, 24, [2, 2]),
-        (27, 27, [2, 2]),
-        (28, 28, [3, 2]),
-        (100, 100, [21, 17])
-    ]
-)
-def test_values(cat_age: int,
-                dog_age: int, human_age: list) -> None:
-    try:
+class TestCatAndDogYearsValues:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, human_age",
+        [
+            (0, 0, [0, 0]),
+            (14, 14, [0, 0]),
+            (15, 15, [1, 1]),
+            (23, 23, [1, 1]),
+            (24, 24, [2, 2]),
+            (27, 27, [2, 2]),
+            (28, 28, [3, 2]),
+            (100, 100, [21, 17])
+        ]
+    )
+    def test_values(self, cat_age: int,
+                    dog_age: int, human_age: list) -> None:
         assert get_human_age(cat_age, dog_age) == human_age
-    except TypeError:
-        "Incorrect data types"
+
+    def test_error(self) -> None:
+        with pytest.raises(TypeError):
+            get_human_age("a", 5)
